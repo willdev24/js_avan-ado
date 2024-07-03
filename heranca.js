0bfb34323e5445e84d57eff37006dd3096f1fc5c
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-07-03 16:54:31 -0400

    heranca

--- heranca.js
+++ heranca.js
@@ -1,4 +1,4 @@
-class dev{
+class dev{     //pai
     constructor(nome,idade,principalLinguagem,stack){
 
           this.nome = nome
@@ -12,9 +12,51 @@ class dev{
  }} 
 
 const dev1 = new dev('willy thiago', 24, 'js', 'full stack')
-const dev2 = new dev('gabriela xavier', 19, 'pyton', 'front end')
-const dev3 = new dev('Gustavo laranjeira', 30, 'ryby', 'moball')
+
 
 dev1.saudac()
-dev2.saudac()
-dev3.saudac()
+
+//filho
+
+class frontEnd  extends dev{
+    constructor(nome,idade,principalLinguagem,stack){
+
+        super()
+
+          this.nome = nome
+          this.idade = idade;
+          this.linguagem = principalLinguagem;
+          this.framework = stack;
+           
+}
+  saudac(){
+    console.log( ` olá, me chamo ${this.nome},  tenho ${this.idade} anos sou dev ${this.framework} e minha principal linguagem e ${this.linguagem} \n` )
+ }} 
+
+
+//filho
+ 
+class backend  extends dev{
+    constructor(nome,idade,principalLinguagem,stack){
+
+        super()
+
+          this.nome = nome
+          this.idade = idade;
+          this.linguagem = principalLinguagem;
+          this.bancoDedados = stack;
+           
+}
+  saudac(){
+    console.log( ` olá, me chamo ${this.nome},  tenho ${this.idade} anos sou dev ${this.bancoDedados} e minha principal linguagem e ${this.linguagem} \n` )
+ }} 
+ 
+ 
+ const front = new dev('willy thiago', 24, 'js', 'react')
+ front.saudac()
+ console.log(front)
+ 
+ 
+ const backends = new dev('willy thiago', 24, 'js', 'mongoDb')
+ backends.saudac()
+ console.log(backends)
